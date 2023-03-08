370c12c327f8cddcc77b2c7b91dd6fefb15a5b61
NyangoyaG <nyangoyagb@gmail.com>
2023-03-08 08:24:57 -0800

    Advanced Task 1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a')-1, -1):
-    print("{0}{1}".format(chr(i), chr(i-32)), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+        print("{}".format(chr(c - i)), end="")
+        i = 32 if i == 0 else 0
+
