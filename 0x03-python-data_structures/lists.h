1f72b8da5536c81e3b5156829765f41ea931be45
NyangoyaG <nyangoyagb@gmail.com>
2023-03-14 10:46:02 -0700

    List File: Linked list palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,14 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for project
  */
 typedef struct listint_s
 {
@@ -21,6 +20,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int checkPalindrome(listint_t **headptr, listint_t *tptr);
 
 #endif /* LISTS_H */
