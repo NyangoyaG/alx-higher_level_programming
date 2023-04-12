fbf2330a443956f912eaf0966e90f382088b2b9c
NyangoyaG <nyangoyagb@gmail.com>
2023-04-12 06:19:10 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """ function that reads a text file (UTF8) and prints it to stdout """
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    except FileNotFoundError:
-        print(f"File {filename} not found.")
+       with open(filename, encoding='utf-8') as fd:
+        for line in fd:
+            print(line, end="")
