0b68300c9083a23d01b485534563471dce2377ef
NyangoyaG <nyangoyagb@gmail.com>
2023-04-12 12:05:23 -0700

    Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,7 +3,8 @@
 
 
 def pascal_triangle(n):
-    """Pascal's Triangle of size n.
+   """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
     """
     if n <= 0:
         return []
