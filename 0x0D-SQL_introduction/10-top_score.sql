3d98667c61372c744d50726dafdbfaf01f673d84
NyangoyaG <nyangoyagb@gmail.com>
2023-05-16 06:44:31 -0700

    List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,3 @@
--- script 10
--- script that lists all records of the table second_table of the database
--- hbtn_0c_0 in your MySQL server.
+-- Script that lists all records of the table second_table of the database
+-- Arranging.
 SELECT score, name FROM second_table ORDER BY score DESC;
